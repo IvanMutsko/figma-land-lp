55b53856fb45a9340ba8cefc678faef34c002522
Ivan Mutsko <imucko1996@gmail.com>
2023-08-31 21:49:16 +0300

    burger fix

--- js/index.js
+++ js/index.js
@@ -2,16 +2,18 @@
 
 const burgerButton = document.getElementById('burger-button');
 const burgerMenu = document.getElementById('burger-menu');
+const backdrop = document.getElementById('backdrop');
 
 burgerButton.addEventListener('click', toggleBurgerMenu);
 
 function toggleBurgerMenu() {
   const elementClass = burgerMenu.getAttribute('class');
-  console.log(elementClass);
   if (elementClass === 'open-menu') {
     burgerMenu.setAttribute('class', 'visually-hidden');
+    backdrop.setAttribute('class', 'visually-hidden');
     return;
   }
   burgerMenu.setAttribute('class', 'open-menu');
+  backdrop.setAttribute('class', 'show');
   return;
 }
